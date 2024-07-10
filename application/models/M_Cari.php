470514c02bbab99e87cd81219b82e69080def563
harkiramadhan <harkiramadhan@gmail.com>
2024-07-10 08:25:18 +0700

    updates

--- application/models/M_Cari.php
+++ application/models/M_Cari.php
@@ -5,6 +5,7 @@ class M_Cari extends CI_Model{
         $this->db->from('buku');
         
         (@$keyword) ? $this->db->like('judul', $keyword) : NULL;
+        (@$keyword) ? $this->db->or_like('pengarang', $keyword) : NULL;
         (@$category_id) ? $this->db->where('JSON_CONTAINS(kategori, \'["' . $category_id . '"]\')', NULL, FALSE) : NULL;
         
         $query = $this->db->get();
