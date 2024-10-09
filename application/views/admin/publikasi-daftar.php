ca12b489e1d559e8b0b9f0972deecb8e6cbdc3c5
harkiramadhan <harkiramadhan@gmail.com>
2024-10-09 15:50:11 +0700

    updates

--- application/views/admin/publikasi-daftar.php
+++ application/views/admin/publikasi-daftar.php
@@ -52,10 +52,35 @@
                                         <div class="btn-group">
                                             <a href="<?= site_url('admin/publikasi/edit/' . md5($row->id)) ?>" class="btn btn-primary btn-sm"><i class="fa-solid fa-pencil"></i></a>
                                             <a href="<?= site_url('publikasi/' . md5($row->id)) ?>" target="__BLANK" class="btn btn-secondary btn-sm mx-1"><i class="fa-solid fa-eye"></i></a>
-                                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#remove"><i class="fa-solid fa-trash"></i></button>
+                                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#remove-<?= md5($row->id) ?>"><i class="fa-solid fa-trash"></i></button>
                                         </div>
                                     </td>
                                 </tr>
+                                <div class="modal fade text-left" id="remove-<?= md5($row->id) ?>" tabindex="-1" aria-labelledby="remove-<?= md5($row->id) ?>" aria-modal="true" role="dialog">
+                                    <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header bg-danger">
+                                                <h5 class="modal-title white" id="myModalLabel120">Hapus Publikasi
+                                                </h5>
+                                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body text-center">
+                                                Hapus Publikasi <br>
+                                                <img src="<?= base_url('assets/img/cover/' . $row->cover)?>" class="rounded-4" height="100px" alt="..."> <br>
+                                                <strong><?= $row->judul ?></strong>
+                                            </div>
+                                            <form action="<?= site_url('admin/publikasi/removePublikasi/' . md5($row->id)) ?>" method="post">
+                                                <input type="hidden" name="judul" value="<?= $row->judul ?>">
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-sm btn-light-secondary me-1 mb-1" data-bs-dismiss="modal">Batalkan</button>
+                                                    <button type="submit" class="btn btn-sm btn-danger me-1 mb-1">Hapus</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             <?php $no++; } ?>
                         </tbody>
                     </table>
